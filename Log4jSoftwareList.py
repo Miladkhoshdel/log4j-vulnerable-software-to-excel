eaa58222f9eaea869799e32d974266f623592f96
Mehdi Bastani <81758792+mehdibastani@users.noreply.github.com>
2021-12-29 12:57:09 +0330

    Update Log4jSoftwareList.py
    According to some changes in source URL, some modifications have been applied to get results. Furthermore, the Excel output is now more elegant.

--- Log4jSoftwareList.py
+++ Log4jSoftwareList.py
@@ -5,22 +5,24 @@
 import requests
 from bs4 import BeautifulSoup
 import xlsxwriter
+from string import ascii_lowercase
 
-page = requests.get("https://github.com/NCSC-NL/log4shell/blob/main/software/README.md")
-soup = BeautifulSoup(page.content, 'html.parser')
+ind = list(ascii_lowercase)
+ind.insert(0, '0-9')
 
-all_tables = soup.find_all('table')
 result = {}
+for i in range(27):
+    page = requests.get(f'https://github.com/NCSC-NL/log4shell/blob/main/software/software_list_{ind[i]}.md')
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-# Remove redundant tables
-all_tables = list(filter(lambda st: "Supplier" in st.find("thead").get_text(), all_tables))
-
-for table in all_tables:
-    whole_table = table.find('tbody')
+    get_table = soup.find('table')
+    whole_table = get_table.find('tbody')
     rows = whole_table.find_all('tr')
+
     for tr in rows:
         whole_tds = tr.find_all('td')
-        if "Not" in whole_tds[3].get_text():
+        tmpList = [whole_tds[i].get_text() for i in range(3, 7)]
+        if "Vulnerable" not in tmpList:
             continue
         if whole_tds[0].get_text() not in result.keys():
             result[whole_tds[0].get_text()] = [whole_tds[1].get_text()]
@@ -31,14 +33,19 @@ row = 0
 column = 0
 
 workbook = xlsxwriter.Workbook('log4J_Vulnerabilities.xlsx')
-worksheet = workbook.add_worksheet()
+bold = workbook.add_format({'bold': True})
 header_list = list(result.keys())
+worksheet = workbook.add_worksheet()
+data_Format1 = workbook.add_format({'border': True, 'bold': True, 'bg_color': '#ff5e5e'})
+data_Format2 = workbook.add_format({'border': True, 'bg_color': '#d9d9d9'})
+data_Format3 = workbook.add_format({'border': True, 'bg_color': '#ffffff'})
+
 for header in header_list:
     row = 0
-    worksheet.write(row, column, header)
+    worksheet.write(row, column, header, data_Format1)
     vuln_list = list(result[header])
     for vulnerability in vuln_list:
         row += 1
-        worksheet.write(row, column, vulnerability)
+        worksheet.write(row, column, vulnerability, data_Format2 if row % 2 == 0 else data_Format3)
     column += 1
 workbook.close()
